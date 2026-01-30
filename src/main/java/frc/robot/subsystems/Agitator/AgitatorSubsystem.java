ecfc36165775e2ab655942e0cb89a4f1aae626b3
tate <tatedale07@gmail.com>
2026-01-30 16:12:07 -0600

    Agitator subsystem and command added

---
+++ src/main/java/frc/robot/subsystems/Agitator/AgitatorSubsystem.java
@@ -0,0 +1,32 @@
+package frc.robot.subsystems.Agitator;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+public class AgitatorSubsystem extends SubsystemBase {
+  private final TalonFX motor = new TalonFX(32);
+  private final VelocityVoltage velocityReq = new VelocityVoltage(0);
+
+  public AgitatorSubsystem() {
+    TalonFXConfiguration cfg = new TalonFXConfiguration();
+
+    cfg.Slot0.kP = 0.1;
+    cfg.Slot0.kI = 0.0;
+    cfg.Slot0.kD = 0.0;
+    cfg.Slot0.kV = 0.1;
+
+    motor.getConfigurator().apply(cfg);
+  }
+
+  // Command motor speed in rotations per second
+  public void setRPS(double rps) {
+    motor.setControl(velocityReq.withVelocity(rps));
+  }
+
+  public void stop() {
+    setRPS(0);
+  }
+    
+}
